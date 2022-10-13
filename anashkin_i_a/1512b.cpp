2af795b55ea09829bee1375a231e97b3699ff8ff
Ilya <ilyaanashkin17@gmail.com>
2022-10-13 13:54:04 +0300

    Update 1512b.cpp

--- anashkin_i_a/1512b.cpp
+++ anashkin_i_a/1512b.cpp
@@ -22,21 +22,31 @@ void solve() {
         X.push_back(stars[1].x);
         Y.push_back(stars[0].y);
         Y.push_back(stars[1].y);
-        for (int i = 0; i < 2; ++i) {
-            for (int j = 0; j < 2; ++j) {
-                field[X[i]][Y[j]] = '*';
-            }
-        }
+
     } else if (stars[0].x != stars[1].x) {
         X.push_back(stars[0].x);
         X.push_back(stars[1].x);
         Y.push_back(stars[0].y);
         if (Y[0] + 1 < n) {
-            Y.push_back(stars[0].y);
+            Y.push_back(stars[0].y + 1);
+        } else {
+            Y.push_back(stars[0].y - 1);
         }
     } else {
         // stars[0].y != stars[1].y
-
+        Y.push_back(stars[0].y);
+        Y.push_back(stars[1].y);
+        X.push_back(stars[0].x);
+        if (X[0] + 1 < n) {
+            X.push_back(stars[0].x + 1);
+        } else {
+            X.push_back(stars[0].x - 1);
+        }
+    }
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            field[X[i]][Y[j]] = '*';
+        }
     }
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
@@ -51,7 +61,6 @@ int main() {
     std::cin >> tests;
     while (tests--) {
         solve();
-        std::cout << "\n";
     }
 
     return 0;
