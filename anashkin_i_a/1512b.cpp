3d448525493ba52b0ce34922d43ba078701dcdce
Ilya <ilyaanashkin17@gmail.com>
2022-10-13 14:10:52 +0300

    1512b memory optimized

--- anashkin_i_a/1512b.cpp
+++ anashkin_i_a/1512b.cpp
@@ -8,12 +8,12 @@ struct coord{
 void solve() {
     int n;
     std::cin >> n;
-    std::vector<std::vector<char>> field(n, std::vector<char>(n));
     std::vector<coord> stars;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            std::cin >> field[i][j];
-            if (field[i][j] == '*') stars.push_back({i, j});
+            char c;
+            std::cin >> c;
+            if (c == '*') stars.push_back({i, j});
         }
     }
     std::vector<int> X, Y;
@@ -33,7 +33,6 @@ void solve() {
             Y.push_back(stars[0].y - 1);
         }
     } else {
-        // stars[0].y != stars[1].y
         Y.push_back(stars[0].y);
         Y.push_back(stars[1].y);
         X.push_back(stars[0].x);
@@ -43,14 +42,13 @@ void solve() {
             X.push_back(stars[0].x - 1);
         }
     }
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            field[X[i]][Y[j]] = '*';
-        }
-    }
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            std::cout << field[i][j];
+            if ((i == X[0] || i == X[1]) && (j == Y[1] || j == Y[0])) {
+                std::cout << '*';
+            } else {
+                std::cout << '.';
+            }
         }
         std::cout << "\n";
     }
