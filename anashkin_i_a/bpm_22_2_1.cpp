75a12ec22d2474750c7fad8c4d7f4390eb490242
Ilya <Ilyaanashkin17@gmail.com>
2022-12-22 14:00:02 +0300

    22.12

---
+++ anashkin_i_a/bpm_22_2_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+	std::cout.precision(5);
+	std::cout << std::fixed;
+	const double kEps = 0.0001;
+	double sum = 0;
+	int i = 0, sign = 1;
+	double x = 0.1, d_x = 0.05;
+	while (1.04 - x >= kEps) {
+		sum += sign * (std::pow(x, 2 * i + 1) / (2 * i + 1));
+		std::cout << x << "\t" << sum << "\n";
+		x += d_x;
+		i += 1;
+		sign *= -1;
+	}
+	return 0;
+}
---
+++ anashkin_i_a/bpm_22_2_2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <numeric>
+
+double Parab(const double& x) {
+	return 2 - x * x + 0.0001;
+}
+
+double Mod(const double& x) {
+	return std::abs(x) - 0.0001;
+}
+
+int main() {
+	const double kEps = std::numeric_limits<double>::epsilon();
+	double x, y;
+	int n_inside_area = 0;
+	int n_all_point = 0;
+	while (std::cin >> x >> y) {
+		if (y - Parab(x) < kEps && y - Mod(x) > kEps) {
+			n_inside_area += 1;
+		}
+		n_all_point += 1;
+		std::cout << n_inside_area << std::endl;
+	}
+	std::cout << 1.0 * n_inside_area / n_all_point;
+	return 0;
+}
