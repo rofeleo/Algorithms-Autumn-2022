bb8b0dda4286f38ae8be4f8cdf25b08fcd5747c7
Ilya <Ilyaanashkin17@gmail.com>
2022-12-24 12:46:55 +0300

    improving control tasks
    как оказалось, не понял условия первой задачи - пришлось переделать
    сделал 3 задачу

--- anashkin_i_a/bpm_22_2_1.cpp
+++ anashkin_i_a/bpm_22_2_1.cpp
@@ -2,18 +2,33 @@
 #include <cmath>
 
 int main() {
+
 	std::cout.precision(5);
 	std::cout << std::fixed;
+
 	const double kEps = 0.0001;
-	double sum = 0;
-	int i = 0, sign = 1;
-	double x = 0.1, d_x = 0.05;
-	while (1.04 - x >= kEps) {
-		sum += sign * (std::pow(x, 2 * i + 1) / (2 * i + 1));
-		std::cout << x << "\t" << sum << "\n";
-		x += d_x;
-		i += 1;
-		sign *= -1;
+
+	double d_x = 0.05;
+	double total_sum = 0;
+	
+	std::cout << "x\t\tsum for x\ttotal sum\n";
+
+	for (double x = 0.1; 1.001 - x > kEps; x += d_x) {
+
+		double cur_sum = 0;
+		int i = 0, sign = 1;
+		double summand = 0;
+
+		do {
+			summand = pow(x, 2 * i + 1) / (2 * i + 1);
+			cur_sum += sign * summand;
+			i += 1;
+			sign *= -1;
+		} while (summand > kEps);
+
+		total_sum += cur_sum;
+		std::cout << x << "\t\t" << cur_sum << "\t\t" << total_sum << std::endl;
 	}
+
 	return 0;
 }
---
+++ anashkin_i_a/bpm_22_2_3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+	int len_word;
+	std::cin >> len_word;
+	std::string word;
+	std::cin >> word;
+	int i_let = len_word - 2;
+	
+	while (i_let >= 0) {
+		std::cout << word[i_let];
+		i_let -= 2;
+	}
+
+	if (len_word % 2) {
+		i_let = 0;
+	} else {
+		i_let = 1;
+	}
+	while (i_let < len_word) {
+		std::cout << word[i_let];
+		i_let += 2;
+	}
+	return 0;
+}
